e6541fbbaa5a1025157b1efeafcde93911fe34ce
Galen Molk <galenmolk@gmail.com>
2022-03-09 17:19:49 -0500

    Removing old Ladder

--- Assets/Scripts/Environment/Ladder.cs
+++ Assets/Scripts/Environment/Ladder.cs
@@ -3,49 +3,8 @@ using UnityEngine;
 
 namespace WormTomb
 {
-    public class Ladder : MonoBehaviour, IInteractable
+    public class Ladder : MonoBehaviour
     {
-        [SerializeField] private float climbDuration;
-        [SerializeField] private PlayerTrigger ladderEndTrigger;
-        [SerializeField] private Transform endTransform;
-        
-        private Vector2 startPos;
-        private bool isClimbing;
-
-        public bool CanInteract()
-        {
-            return true;
-        }
-
-        public void Interact()
-        {
-            StartCoroutine(ClimbLadder());
-        }
-
-        private IEnumerator ClimbLadder()
-        {
-            isClimbing = true;
-            Player.Instance.RB.MoveTo(startPos);
-            Vector2 distance = (Vector2)endTransform.position - startPos;
-            Vector2 speed = distance / climbDuration;
-
-            while (isClimbing)
-            {
-                Player.Instance.RB.MoveTo((Vector2)Player.Instance.Transform.position + speed * Time.deltaTime);
-                yield return YieldRegistry.waitForFixedUpdate;
-            }
-        }
-
-        private void Awake()
-        {
-            ladderEndTrigger.OnPlayerTriggered.AddListener(EndClimbing);
-            startPos = transform.position;
-        }
-
-        private void EndClimbing()
-        {
-            Debug.Log("EndClimbing");
-            isClimbing = false;
-        }
+       
     }
 }
